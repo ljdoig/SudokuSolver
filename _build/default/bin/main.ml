fd7796d03f6b93eb1b63e31506534c3314b405f3
ldoig <lachjd@protonmail.com>
2024-01-08 23:26:42 +0100

    ignore _build

--- _build/default/bin/main.ml
+++
@@ -1,7 +0,0 @@
-let () =
-  match Sudoku.read "test.txt" with
-  | None -> print_endline "illegal sudoku"
-  | Some sudoku -> Sudoku.print sudoku
-  (* match Sudoku.solve sudoku with
-  | None -> print_endline "No solution"
-  | Some solved_sudoku -> Sudoku.print solved_sudoku *)
--- _build/default/lib/sudoku.mli
+++
@@ -1,7 +0,0 @@
-type t
-
-val create: int option array array -> t option
-val to_string: t -> string
-val read: string -> t option
-val write: t -> string -> unit
-val print: t -> unit
