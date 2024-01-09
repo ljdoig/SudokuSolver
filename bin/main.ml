8abe79994f7a633750fa80040788e8ebfe94dca2
ldoig <lachjd@protonmail.com>
2024-01-09 08:58:45 +0100

    command line arg

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,15 @@
-let () =
-  match Sudoku.read "sudokus/unsolveable1.txt" with
+open Core
+
+let solve_sudoku filename =
+  match Sudoku.read filename with
+  | Some sudoku -> Sudoku.solve sudoku
   | None -> print_endline "illegal sudoku"
-  | Some sudoku -> Sudoku.print sudoku; Sudoku.solve sudoku
+
+let command =
+  Command.basic
+    ~summary:"Solve a sudoku from a textfile"
+    (let%map_open.Command filename = anon ("filename" %: Filename_unix.arg_type)
+     in
+     fun () -> solve_sudoku filename)
+
+let () = Command_unix.run ~version:"1.0" ~build_info:"sudoku" command
