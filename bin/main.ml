dbae63ff5c59f1ee06e7d5ce69c9c7cbe3dac7ff
ldoig <lachjd@protonmail.com>
2024-01-14 16:59:32 +0100

    add timer to solver

--- bin/main.ml
+++ bin/main.ml
@@ -2,7 +2,15 @@ open Core
 
 let solve_sudoku filename =
   match Sudoku.read filename with
-  | Some sudoku -> Sudoku.solve ~verbose:true sudoku |> ignore
+  | Some sudoku ->
+    begin
+      let start_time = Time.now () in
+      Sudoku.solve ~verbose:true sudoku |> ignore;
+      let end_time = Time.now () in
+      Time.diff end_time start_time
+        |> Time.Span.to_sec
+        |> Printf.printf "Solving took: %f seconds\n"
+    end
   | None -> print_endline "illegal sudoku"
 
 let command =
